6ef25f96e1bb1f0143eea14c73923fc89c4c11cc
mjhosseini <hosseini.mohammadjavad@gmail.com>
2022-02-20 13:59:14 +0000

    Update readme

--- scripts/data.sh
+++ scripts/data.sh
@@ -2,6 +2,6 @@
 fileid=1lGqeBUL2JKlAclvsqNfwwR9NB6wl5uEH
 filename="data.tar.gz"
 html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o ${filename}
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|perl -nle'print $& while m{(confirm=[a-zA-Z0-9\-_]+)}g'`&id=${fileid}" -o ${filename}
 tar -xvzf data.tar.gz
 rm cookie
