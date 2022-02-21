bddd60d018e58e324991e4a6192f6ed855be5388
mjhosseini <hosseini.mohammadjavad@gmail.com>
2022-02-21 00:26:58 +0000

    Update pre-processing

--- scripts/dl_pretrained.sh
+++ scripts/dl_pretrained.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 fileid=1VdeY62BjWS4bERKU-v-nnEu96IzjedQL
-filename="pretrained_models"
+filename="pretrained_models.tar.gz"
 html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|perl -nle'print $& while m{(confirm=[a-zA-Z0-9\-_]+)}g'`&id=${fileid}" -o ${filename}
 tar -xvzf pretrained_models.tar.gz
