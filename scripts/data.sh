6fa577da28dea438fd3c2871ae81a77db5388def
mjhosseini <hosseini.mohammadjavad@gmail.com>
2022-07-12 10:51:35 +0100

    Update relations folder

--- scripts/data.sh
+++ scripts/data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-fileid=1lGqeBUL2JKlAclvsqNfwwR9NB6wl5uEH
+fileid=1d93CTsJ9OG0I68fHHWvX8iVkSefCmkyq
 filename="data.tar.gz"
 html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|perl -nle'print $& while m{(confirm=[a-zA-Z0-9\-_]+)}g'`&id=${fileid}" -o ${filename}
